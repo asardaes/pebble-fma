a1131bfd9db6503cfeeaf7913fef4b77861670df
asardaes <alexis.sarda@gmail.com>
2016-04-27 13:29:02 +0200

    Changed basalt animation delay
    I can't tell for sure if it's faster or slower...

--- src/basalt.c
+++ src/basalt.c
@@ -23,7 +23,7 @@ GBitmap *bg_bitmap;
 GBitmap *hands_bitmap_0, *hands_bitmap_1, *hands_bitmap_2;
 GBitmap *rune_bitmap, *charge_bitmap;
 
-uint32_t anim_duration = 80;
+uint32_t anim_duration = 100;
 
 /* ===================================================================================================================== */
 
