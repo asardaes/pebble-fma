0c1102fe157b44c92f633aed3ecb91d2db00f5fb
asardaes <alexis.sarda@gmail.com>
2018-01-30 17:41:16 +0100

    Fix compilation with comiler v7

--- src/common.c
+++ src/common.c
@@ -18,7 +18,7 @@ static Window *s_main_window;
 
 static GBitmap *s_sparks_bitmap_1, *s_sparks_bitmap_2;
 
-static char temperature[5];
+static char temperature[12];
 
 static int anim_index = 1;
 static int anim_queue = 0;
@@ -40,7 +40,7 @@ char time_buffer[] = "00:00";
 
 static void update_time();
 
-void debug() {	
+void debug() {
 	static int test = 7;
 	snprintf(temperature, sizeof(temperature), "%d", test);
 	text_layer_set_text(temp_layer, temperature);
@@ -77,7 +77,7 @@ static void get_weather() {
 
 	// Begin dictionary
 	DictionaryIterator *iter;
-	
+
 	AppMessageResult res = app_message_outbox_begin(&iter);
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "AppMsgBegin: %s", translate_error(res));
 
@@ -103,21 +103,21 @@ static void tap_handler_exit(void *data) {
 	bg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
 	bitmap_layer_set_bitmap(bg_layer, bg_bitmap);
 	text_layer_set_text(message_layer, "");
-	
+
 	next_animation();
 }
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
 	if (anim_queue == 0) {
 		APP_LOG(APP_LOG_LEVEL_DEBUG, "Entering tap_handler");
-	
+
 		if (bg_bitmap) gbitmap_destroy(bg_bitmap);
-	
+
 		bg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG_CIRCLE);
 		bitmap_layer_set_bitmap(bg_layer, bg_bitmap);
-	
+
 		text_layer_set_text(message_layer, "Don't\nforget\n3.oct.11");
-	
+
 		app_timer_register(5000, tap_handler_exit, NULL);
 		anim_queue++;
 	}
@@ -127,30 +127,30 @@ static void tap_handler(AccelAxisType axis, int32_t direction) {
 
 void handle_bt(bool connected) {
 	if (rune_bitmap) gbitmap_destroy(rune_bitmap);
-	
+
 	if (connected) {
 		rune_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT);
 	} else {
 		rune_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_NOBT);
 	}
-	
+
 	bitmap_layer_set_bitmap(rune_layer, rune_bitmap);
 }
 
 void handle_battery(BatteryChargeState charge_state) {
 	if (charge_bitmap) gbitmap_destroy(charge_bitmap);
-	
+
 	if (charge_state.is_charging) {
 		charge_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CHARGE);
 		snprintf(batt_buffer, sizeof(batt_buffer), "%s", "");
-		
+
 	} else {
 		charge_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CIRCLE);
 		snprintf(batt_buffer, sizeof(batt_buffer), "%d", charge_state.charge_percent);
 	}
 
 	PBL_IF_COLOR_ELSE(layer_mark_dirty(batt_layer_b), text_layer_set_text(batt_layer, batt_buffer));
-	
+
 	bitmap_layer_set_bitmap(charge_layer, charge_bitmap);
 }
 
@@ -158,7 +158,7 @@ void handle_battery(BatteryChargeState charge_state) {
 
 static void update_time() {
 	// Get a tm structure
-	time_t temp = time(NULL); 
+	time_t temp = time(NULL);
 	struct tm *tick_time = localtime(&temp);
 
 	// Write the current hours and minutes into the buffer
@@ -169,7 +169,7 @@ static void update_time() {
 		// Use 12 hour format
 		strftime(time_buffer, sizeof(time_buffer), "%I:%M", tick_time);
 	}
-	
+
 	if (dbg) {
 		snprintf(time_buffer, sizeof(time_buffer), "%s", "12:34");
 	} else if (connection_service_peek_pebble_app_connection() && weather_flag) {
@@ -177,7 +177,7 @@ static void update_time() {
 	}
 
 	PBL_IF_COLOR_ELSE(layer_mark_dirty(time_layer_b), text_layer_set_text(time_layer, time_buffer));
-	
+
 	// Date
 	strftime(date_buffer, sizeof(date_buffer), "%b, %a %d", tick_time);
 
@@ -190,7 +190,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 		weather_flag = 1;
 		text_layer_set_text(temp_layer, "...");
 	}
-	
+
 	if (anim_queue == 0) {
 		anim_queue++;
 		next_animation();
@@ -202,7 +202,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void next_animation() {
 	//APP_LOG(APP_LOG_LEVEL_DEBUG, "Animation %d", anim_index);
 	bool stop = false;
-	
+
 	switch (anim_index) {
 		case 0:
 			layer_set_hidden(bitmap_layer_get_layer(hands_layer), false);
@@ -215,7 +215,7 @@ static void next_animation() {
 			bitmap_layer_set_bitmap(hands_layer, hands_bitmap_1);
 			anim_index++;
 		break;
-		
+
 		case 2:
 			s_sparks_bitmap_1 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SPARKS_1);
 			bitmap_layer_set_bitmap(hands_layer, hands_bitmap_2);
@@ -231,7 +231,7 @@ static void next_animation() {
 			anim_duration += 25;
 			anim_index++;
 		break;
-		
+
 		case 3:
 			psleep(PBL_IF_COLOR_ELSE(175, 200));
 
@@ -242,7 +242,7 @@ static void next_animation() {
 
 			anim_index++;
 		break;
-		
+
 		case 4:
 			bitmap_layer_set_bitmap(sparks_layer, s_sparks_bitmap_2);
 
@@ -251,7 +251,7 @@ static void next_animation() {
 
 			anim_index++;
 		break;
-		
+
 		case 5:
 			bitmap_layer_set_bitmap(sparks_layer, s_sparks_bitmap_1);
 			bitmap_layer_set_bitmap(hands_layer, hands_bitmap_1);
@@ -261,7 +261,7 @@ static void next_animation() {
 
 			anim_index++;
 		break;
-		
+
 		case 6:
 			bitmap_layer_set_bitmap(sparks_layer, s_sparks_bitmap_2);
 			bitmap_layer_set_bitmap(hands_layer, hands_bitmap_0);
@@ -291,7 +291,7 @@ static void next_animation() {
 				update_time();
 			}
 		break;
-		
+
 		default:
 			layer_set_hidden(bitmap_layer_get_layer(hands_layer), true);
 			anim_index = 0;
@@ -303,7 +303,7 @@ static void next_animation() {
 				update_time();
 		break;
 	}
-	
+
 	if (!stop) {
 		if (dbg)
 			app_timer_register(5000, next_animation, NULL);
@@ -320,7 +320,7 @@ static void next_animation() {
 void inbox_received_callback(DictionaryIterator *iterator, void *context) {
 	// Read first item
 	Tuple *t = dict_read_first(iterator);
-	
+
 	// For all items
 	while(t != NULL) {
 		// Which key was received?
@@ -333,11 +333,11 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
 					snprintf(temperature, sizeof(temperature), "%d", (int)t->value->int32);
 				}
 				text_layer_set_text(temp_layer, temperature);
-			
+
 				// Mark that weather shouldn't be updated any more
-				weather_flag = 0; 
+				weather_flag = 0;
 			break;
-			
+
 			case UNIT_TEMPERATURE:
 				if (strcmp(t->value->cstring, "C") == 0) {
 					persist_write_bool(UNIT_TEMPERATURE, false);
@@ -348,7 +348,7 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
 					temp_unit = 1;
 					APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting temperature units to fahrenheit");
 				}
-			
+
 				text_layer_set_text(temp_layer, "...");
 				weather_flag = 1;
 				get_weather();
@@ -368,7 +368,7 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
 					if (anim_queue == 0) anim_index = 0;
 				}
 			break;
-			
+
 			default:
 				APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
 			break;
@@ -399,7 +399,7 @@ void init() {
 
 	if (persist_exists(HANDS))
 		show_hands = persist_read_bool(HANDS);
-	
+
 	// Create main Window element and assign to pointer
 	s_main_window = window_create();
 
@@ -411,32 +411,32 @@ void init() {
 
 	// Show the Window on the watch, with animated=true
 	window_stack_push(s_main_window, true);
-	
+
 	// Register bluetooth service
 	connection_service_subscribe((ConnectionHandlers) {
 		.pebble_app_connection_handler = handle_bt,
 		NULL
 	});
-	
+
 	// Register battery service
 	battery_state_service_subscribe(handle_battery);
-	
+
 	// Register with TickTimerService
 	if (!dbg) tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-	
+
 	// Subscribe to the accelerometer tap service
 	accel_tap_service_subscribe(tap_handler);
-	
+
 	// Register callbacks
 	app_message_register_inbox_received(inbox_received_callback);
 	app_message_register_inbox_dropped(inbox_dropped_callback);
 	app_message_register_outbox_failed(outbox_failed_callback);
 	app_message_register_outbox_sent(outbox_sent_callback);
-	
+
 	// Open AppMessage
 	AppMessageResult res = app_message_open(APP_MESSAGE_INBOX_SIZE_MINIMUM, APP_MESSAGE_OUTBOX_SIZE_MINIMUM);
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "AppMsgOpen: %s", translate_error(res));
-	
+
 	if (dbg)
 		debug();
 	else {
